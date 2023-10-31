08ca8207e4bf16582a151c1c857f7b5f8724c6f7
Hycarus <mirkomarasco08@gmail.com>
2023-10-31 15:10:32 +0100

    added tasks array

--- js/script.js
+++ js/script.js
@@ -4,7 +4,29 @@ createApp({
     // contiene tutti i dati / le variabili 
     data(){
         return {
-            
+            tasks: [
+                {
+                    id: 1,
+                    text: 'testo 1',
+                    done: false,
+                }, 
+                {
+                    id: 2,
+                    text: 'testo 2',
+                    done: true,
+                }, 
+                {
+                    id: 3,
+                    text: 'testo 3',
+                    done: false,
+                },
+                {
+                    id: 4,
+                    text: 'testo 4',
+                    done: true,
+                },
+            ],
+            lastId: 4,
         }
     },
     // contiene le funzioni e i metodi 
