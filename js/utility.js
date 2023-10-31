f31c50b886e37292913a8a072701000fb107e257
Hycarus <mirkomarasco08@gmail.com>
2023-10-31 15:25:35 +0100

    added function getIndex utility

--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,6 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
   }
+function getIndex(id, array){
+    return array.findIndex((el) => el.id === id);
+}
